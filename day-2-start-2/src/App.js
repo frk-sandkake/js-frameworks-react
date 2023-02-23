a02333762b190c53910af029061ecf9733a57e53
FRK-SANDKAKE\linda <frk.sandkake@gmail.com>
2023-02-23 14:12:14 +0100

    rm wrong code, create heading & paragraph components w props

--- day-2-start-2/src/App.js
+++ day-2-start-2/src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import Heading from "./components/Heading";
+import Paragraph from "./components/Paragraph";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Heading
+          name="Jane"
+          age="40"
+          color="purple"
+          fontSize="28"
+      />
+      <Paragraph
+          content="Let's be the change we wish to see in the world"
+      />
     </div>
   );
 }
