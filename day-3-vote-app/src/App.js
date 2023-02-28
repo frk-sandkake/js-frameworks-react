4bceb06073c5a9a107262576d3f401f2b7f1b21b
FRK-SANDKAKE\linda <frk.sandkake@gmail.com>
2023-02-28 13:01:35 +0100

    vote app day 3

--- day-3-vote-app/src/App.js
+++ day-3-vote-app/src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
+import React, {useState} from "react";
 import './App.css';
+import Heading from "./components/Heading";
+import JohnnyVote from "./voteComponent/JohnnyVote";
+import JackVote from "./voteComponent/JackVote";
+import VotingBtn from "./components/VotingBtn";
 
 function App() {
+    const [jackVote, setJackVote] = useState(0);
+    const [johnnyVote, setJohnnyVote] = useState(0);
+
+    function handleVotes(name) {
+        if (name === "Johnny") {
+            setJohnnyVote(johnnyVote + 1);
+        }if (name === "Jack") {
+            setJackVote(jackVote + 1);
+        }
+    }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+          <Heading color="purple" fontSize="50" />
       </header>
+        <main>
+            <JohnnyVote />
+            <VotingBtn handleVote={} name="Johnny" />
+            <JackVote />
+            <VotingBtn name="Jack" />
+        </main>
     </div>
   );
 }
---
+++ day-3-vote-app/src/voteComponent/JohnnyVote.js
@@ -0,0 +1,12 @@
+import johnny from "../assets/johnny.jpg";
+
+function JohnnyVote() {
+    return (
+        <div>
+            <h2>Heeerre's Johnny!</h2>
+            <img src={johnny} alt="here's johnny" />
+        </div>
+    );
+}
+
+export default JohnnyVote;
