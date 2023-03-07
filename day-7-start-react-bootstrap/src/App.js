bd69b48f7a20de6deee57f32b4165562f01ae337
FRK-SANDKAKE\linda <frk.sandkake@gmail.com>
2023-03-07 12:59:55 +0100

    testing react-bootstrap

--- day-7-start-react-bootstrap/src/App.js
+++ day-7-start-react-bootstrap/src/App.js
@@ -1,9 +1,10 @@
+import Layout from "./components/layout/Layout";
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-    </div>
+   <Layout/>
   );
 }
 
