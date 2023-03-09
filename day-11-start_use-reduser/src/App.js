8928580ec5ad91d4eda84035ac5a6b50f679d5a7
FRK-SANDKAKE\linda <frk.sandkake@gmail.com>
2023-03-09 17:58:14 +0100

    created counter with useReducter

---
+++ day-11-start_use-reduser/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import HandleClick from "./components/BasicExample";
+
+function App() {
+  return (
+    <div className="App">
+     <HandleClick/>
+    </div>
+  );
+}
+
+export default App;
